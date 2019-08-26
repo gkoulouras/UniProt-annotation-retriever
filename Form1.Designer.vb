c8c10bd3ccfefb237cbb31376a316114fc9719ac
Grigorios Koulouras <mail@grigoriskoulouras.com>
2019-08-26 11:03:54 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SelectFileBtn = New System.Windows.Forms.Button()
+        Me.FilePathTxtBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LocationChckBox = New System.Windows.Forms.CheckBox()
+        Me.TopologyChckBox = New System.Windows.Forms.CheckBox()
+        Me.MolProcChckBox = New System.Windows.Forms.CheckBox()
+        Me.StartBtn = New System.Windows.Forms.Button()
+        Me.StopBtn = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.LogTxtBox = New System.Windows.Forms.TextBox()
+        Me.version_lbl = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ErrorLogTxtBox = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'SelectFileBtn
+        '
+        Me.SelectFileBtn.Location = New System.Drawing.Point(114, 84)
+        Me.SelectFileBtn.Name = "SelectFileBtn"
+        Me.SelectFileBtn.Size = New System.Drawing.Size(127, 30)
+        Me.SelectFileBtn.TabIndex = 0
+        Me.SelectFileBtn.Text = "Select File"
+        Me.SelectFileBtn.UseVisualStyleBackColor = True
+        '
+        'FilePathTxtBox
+        '
+        Me.FilePathTxtBox.Location = New System.Drawing.Point(7, 58)
+        Me.FilePathTxtBox.Name = "FilePathTxtBox"
+        Me.FilePathTxtBox.Size = New System.Drawing.Size(334, 20)
+        Me.FilePathTxtBox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(111, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select a list of UniProt IDs"
+        '
+        'LocationChckBox
+        '
+        Me.LocationChckBox.AutoSize = True
+        Me.LocationChckBox.Checked = True
+        Me.LocationChckBox.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.LocationChckBox.Location = New System.Drawing.Point(7, 142)
+        Me.LocationChckBox.Name = "LocationChckBox"
+        Me.LocationChckBox.Size = New System.Drawing.Size(67, 17)
+        Me.LocationChckBox.TabIndex = 3
+        Me.LocationChckBox.Text = "Location"
+        Me.LocationChckBox.UseVisualStyleBackColor = True
+        '
+        'TopologyChckBox
+        '
+        Me.TopologyChckBox.AutoSize = True
+        Me.TopologyChckBox.Checked = True
+        Me.TopologyChckBox.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.TopologyChckBox.Location = New System.Drawing.Point(7, 168)
+        Me.TopologyChckBox.Name = "TopologyChckBox"
+        Me.TopologyChckBox.Size = New System.Drawing.Size(70, 17)
+        Me.TopologyChckBox.TabIndex = 4
+        Me.TopologyChckBox.Text = "Topology"
+        Me.TopologyChckBox.UseVisualStyleBackColor = True
+        '
+        'MolProcChckBox
+        '
+        Me.MolProcChckBox.AutoSize = True
+        Me.MolProcChckBox.Checked = True
+        Me.MolProcChckBox.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.MolProcChckBox.Location = New System.Drawing.Point(7, 191)
+        Me.MolProcChckBox.Name = "MolProcChckBox"
+        Me.MolProcChckBox.Size = New System.Drawing.Size(124, 17)
+        Me.MolProcChckBox.TabIndex = 5
+        Me.MolProcChckBox.Text = "Molecule Processing"
+        Me.MolProcChckBox.UseVisualStyleBackColor = True
+        '
+        'StartBtn
+        '
+        Me.StartBtn.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.StartBtn.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.StartBtn.Location = New System.Drawing.Point(63, 296)
+        Me.StartBtn.Name = "StartBtn"
+        Me.StartBtn.Size = New System.Drawing.Size(210, 23)
+        Me.StartBtn.TabIndex = 6
+        Me.StartBtn.Text = "Start"
+        Me.StartBtn.UseVisualStyleBackColor = False
+        '
+        'StopBtn
+        '
+        Me.StopBtn.BackColor = System.Drawing.Color.SeaShell
+        Me.StopBtn.Location = New System.Drawing.Point(63, 325)
+        Me.StopBtn.Name = "StopBtn"
+        Me.StopBtn.Size = New System.Drawing.Size(210, 23)
+        Me.StopBtn.TabIndex = 7
+        Me.StopBtn.Text = "Stop"
+        Me.StopBtn.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.StartBtn)
+        Me.GroupBox1.Controls.Add(Me.StopBtn)
+        Me.GroupBox1.Controls.Add(Me.SelectFileBtn)
+        Me.GroupBox1.Controls.Add(Me.FilePathTxtBox)
+        Me.GroupBox1.Controls.Add(Me.MolProcChckBox)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TopologyChckBox)
+        Me.GroupBox1.Controls.Add(Me.LocationChckBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 33)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(364, 365)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Application parameters"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.LogTxtBox)
+        Me.GroupBox2.Location = New System.Drawing.Point(382, 33)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(459, 218)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Application logs"
+        '
+        'LogTxtBox
+        '
+        Me.LogTxtBox.Location = New System.Drawing.Point(6, 19)
+        Me.LogTxtBox.Multiline = True
+        Me.LogTxtBox.Name = "LogTxtBox"
+        Me.LogTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.LogTxtBox.Size = New System.Drawing.Size(447, 189)
+        Me.LogTxtBox.TabIndex = 0
+        '
+        'version_lbl
+        '
+        Me.version_lbl.AutoSize = True
+        Me.version_lbl.Location = New System.Drawing.Point(746, 413)
+        Me.version_lbl.Name = "version_lbl"
+        Me.version_lbl.Size = New System.Drawing.Size(104, 13)
+        Me.version_lbl.TabIndex = 10
+        Me.version_lbl.Text = "version 1.02.190619"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ErrorLogTxtBox)
+        Me.GroupBox3.Location = New System.Drawing.Point(382, 257)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(459, 141)
+        Me.GroupBox3.TabIndex = 11
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Error logs"
+        '
+        'ErrorLogTxtBox
+        '
+        Me.ErrorLogTxtBox.ForeColor = System.Drawing.Color.Firebrick
+        Me.ErrorLogTxtBox.Location = New System.Drawing.Point(6, 19)
+        Me.ErrorLogTxtBox.Multiline = True
+        Me.ErrorLogTxtBox.Name = "ErrorLogTxtBox"
+        Me.ErrorLogTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.ErrorLogTxtBox.Size = New System.Drawing.Size(447, 116)
+        Me.ErrorLogTxtBox.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(862, 435)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.version_lbl)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "UniProt annotation downloader"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SelectFileBtn As Button
+    Friend WithEvents FilePathTxtBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LocationChckBox As CheckBox
+    Friend WithEvents TopologyChckBox As CheckBox
+    Friend WithEvents MolProcChckBox As CheckBox
+    Friend WithEvents StartBtn As Button
+    Friend WithEvents StopBtn As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents LogTxtBox As TextBox
+    Friend WithEvents version_lbl As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ErrorLogTxtBox As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,266 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Net
+Imports System.Xml
+
+Public Class Form1
+
+    Dim filePath As String
+    Dim directoryPath As String
+    Dim stopClickPressed As Boolean = False
+    'Dim currentdate = Date.Now.ToString("ddMMMyyyy")
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub SelectFileBtn_Click(sender As Object, e As EventArgs) Handles SelectFileBtn.Click
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+        If result = DialogResult.OK Then
+            filePath = OpenFileDialog1.FileName
+            directoryPath = Path.GetDirectoryName(filePath)
+            Try
+                FilePathTxtBox.Text = filePath
+            Catch ex As Exception
+                MessageBox.Show("The upload process was unsuccessful. Please try again", "Critical Error")
+            End Try
+        End If
+    End Sub
+
+    Public Sub StartBtn_Click(sender As Object, e As EventArgs) Handles StartBtn.Click
+        Dim proteinIDs As New DataTable
+        Dim proteinAnnot As New DataTable
+        LogTxtBox.Clear()
+
+        If filePath <> "" Then
+            If LocationChckBox.Checked = False And TopologyChckBox.Checked = False And MolProcChckBox.Checked = False Then
+                MessageBox.Show("Please select at least one feature", "Warning message")
+            Else
+                Try
+                    proteinIDs.Columns.Add("protID", Type.GetType("System.String"))
+
+                    proteinAnnot.Columns.Add("uniprotID", Type.GetType("System.String"))
+                    proteinAnnot.Columns.Add("location", Type.GetType("System.String"))
+                    proteinAnnot.Columns.Add("topology", Type.GetType("System.String"))
+                    proteinAnnot.Columns.Add("molecule_processing", Type.GetType("System.String"))
+
+                    Dim sr As New IO.StreamReader(filePath)
+
+                    Dim s As String = String.Empty
+                    Do While Not sr.EndOfStream
+                        s = sr.ReadLine
+                        Dim protID As DataRow = proteinIDs.NewRow()
+                        protID("protID") = s
+                        proteinIDs.Rows.Add(protID)
+                    Loop
+
+                    Dim totalCounter As Integer = proteinIDs.Rows.Count
+                    Dim iter As Integer = 0
+                    If totalCounter > 0 Then
+                        stopClickPressed = False
+                        Do While ((stopClickPressed = False) And (iter < totalCounter))
+
+                            Application.DoEvents()
+                            LogTxtBox.AppendText("The record " + proteinIDs.Rows(iter).Item(0).ToString + " is currently processed" + " (" + (iter + 1).ToString + "  out of " + (totalCounter).ToString + ")" + vbNewLine)
+
+                            Dim proteinID As String = proteinIDs.Rows(iter).Item(0)
+                            Dim type As String = String.Empty
+                            Dim type2 As String = String.Empty
+                            Dim location As String = String.Empty
+                            Dim topology As String = String.Empty
+                            Dim mol_processing As String = String.Empty
+
+                            Try
+
+
+                                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+                                Dim request As String = "https://www.uniprot.org/uniprot/" & proteinID & ".xml"
+                                Dim doc As New XmlDocument
+                                doc.Load(request)
+
+                                location = String.Empty
+                                topology = String.Empty
+                                mol_processing = String.Empty
+
+                                For Each entry As XmlElement In doc.GetElementsByTagName("entry")
+                                    For Each feature As XmlElement In entry.GetElementsByTagName("comment")
+
+                                        type = GetAttibuteValue(feature, "type")
+
+                                        If type = "subcellular location" Then
+
+                                            For Each loc As XmlElement In feature.GetElementsByTagName("location")
+                                                location = location & ";" & loc.InnerText
+                                            Next
+
+                                            For Each top As XmlElement In feature.GetElementsByTagName("topology")
+                                                topology = topology & ";" & top.InnerText
+                                            Next
+
+                                        End If
+                                    Next
+
+                                    If Len(location) > 0 Then
+                                        location = location.Substring(1, Len(location) - 1)
+                                        Dim locationlist As New List(Of String)(location.Split(";"c))
+                                        locationlist = locationlist.Distinct().ToList()
+                                        locationlist.Sort()
+                                        location = String.Join(" ; ", locationlist)
+                                    End If
+                                    If Len(topology) > 0 Then
+                                        topology = topology.Substring(1, Len(topology) - 1)
+                                        Dim topologylist As New List(Of String)(topology.Split(";"c))
+                                        topologylist = topologylist.Distinct().ToList()
+                                        topologylist.Sort()
+                                        topology = String.Join(" ; ", topologylist)
+                                    End If
+
+                                    For Each feature2 As XmlElement In entry.GetElementsByTagName("feature")
+                                        type2 = GetAttibuteValue(feature2, "type")
+
+                                        If type2 = "initiator methionine" Or type2 = "signal peptide" Or type2 = "transit peptide" Or type2 = "propeptide" Or type2 = "chain" Or type2 = "peptide" Then
+                                            mol_processing = mol_processing & ";" & type2
+                                        End If
+
+                                    Next
+                                    If Len(mol_processing) > 0 Then
+                                        mol_processing = mol_processing.Substring(1, Len(mol_processing) - 1)
+                                        Dim mol_processinglist As New List(Of String)(mol_processing.Split(";"c))
+                                        mol_processinglist = mol_processinglist.Distinct().ToList()
+                                        mol_processinglist.Sort()
+                                        mol_processing = String.Join(" ; ", mol_processinglist)
+                                    End If
+
+                                Next
+
+                                proteinAnnot.Rows.Add(proteinID, location, topology, mol_processing)
+                                iter += 1
+
+                            Catch ex As Exception
+                                ErrorLogTxtBox.AppendText("The following error occured while processing the " & proteinID & ": " & ex.Message & vbNewLine)
+                                iter += 1
+                            End Try
+
+                        Loop
+
+                        If stopClickPressed = False Then
+                            Dim file As System.IO.StreamWriter
+                            Dim utf8WithoutBom As New System.Text.UTF8Encoding(False)
+                            If proteinAnnot.Rows.Count > 0 Then
+                                file = My.Computer.FileSystem.OpenTextFileWriter(directoryPath + "\proteinAnnotations.txt", False, utf8WithoutBom)
+                                Dim headerLine1 As String = String.Empty
+                                Dim headerLine2 As String = String.Empty
+                                Dim mainTxt As String = String.Empty
+
+                                headerLine1 = "UniprotID"
+                                If LocationChckBox.Checked = True Then
+                                    headerLine1 &= "" + vbTab + "Location"
+                                End If
+                                If TopologyChckBox.Checked = True Then
+                                    headerLine1 &= "" + vbTab + "Topology"
+                                End If
+                                If MolProcChckBox.Checked = True Then
+                                    headerLine1 &= "" + vbTab + "Molecule processing"
+                                End If
+                                'file.WriteLine("UniprotID" + vbTab + "Location" + vbTab + "Topology" + vbTab + "Molecule processing")
+                                file.WriteLine(headerLine1)
+
+                                headerLine2 = "#!{Type}"
+                                If LocationChckBox.Checked = True Then
+                                    headerLine2 = headerLine2 & "" + vbTab + "Categorical"
+                                End If
+                                If TopologyChckBox.Checked = True Then
+                                    headerLine2 = headerLine2 & "" + vbTab + "Categorical"
+                                End If
+                                If MolProcChckBox.Checked = True Then
+                                    headerLine2 = headerLine2 & "" + vbTab + "Categorical"
+                                End If
+                                'file.WriteLine("#!{Type}" + vbTab + "Categorical" + vbTab + "Categorical" + vbTab + "Categorical")
+                                file.WriteLine(headerLine2)
+
+                                For i As Integer = 0 To proteinAnnot.Rows.Count - 1
+                                    mainTxt = proteinAnnot.Rows(i).Item("uniprotID")
+                                    If LocationChckBox.Checked = True Then
+                                        mainTxt = mainTxt + vbTab + proteinAnnot.Rows(i).Item("location")
+                                    End If
+                                    If TopologyChckBox.Checked = True Then
+                                        mainTxt = mainTxt + vbTab + proteinAnnot.Rows(i).Item("topology")
+                                    End If
+                                    If MolProcChckBox.Checked = True Then
+                                        mainTxt = mainTxt + vbTab + proteinAnnot.Rows(i).Item("molecule_processing")
+                                    End If
+                                    'file.WriteLine(proteinAnnot.Rows(i).Item("uniprotID") + vbTab + proteinAnnot.Rows(i).Item("location") + vbTab + proteinAnnot.Rows(i).Item("topology") + vbTab + proteinAnnot.Rows(i).Item("molecule_processing"))
+                                    file.WriteLine(mainTxt)
+                                Next
+                                file.Close()
+
+                                Dim directorySelected As DirectoryInfo = New DirectoryInfo(directoryPath)
+                                Compress(directorySelected, directoryPath)
+
+                                LogTxtBox.AppendText(vbNewLine + "The annotation file has been created successfully in the following directory: " + vbNewLine)
+                                LogTxtBox.AppendText(directoryPath + vbNewLine)
+                                LogTxtBox.AppendText(vbNewLine + vbNewLine + "The application terminated successfully")
+                            End If
+                        Else
+                            LogTxtBox.AppendText(vbNewLine + vbNewLine + "The application forcely stopped by the user" + vbNewLine)
+                        End If
+                    Else
+                        MessageBox.Show("An error occured while reading the list of proteins. Please check that the file contains a list of UniProt IDs only.", "Warning message")
+                    End If
+
+                Catch ex As Exception
+                    ErrorLogTxtBox.AppendText(vbNewLine + "An unexpected error occured. The application stopped." + vbNewLine)
+                    MessageBox.Show(ex.Message, "Critical error")
+                End Try
+            End If
+        Else
+            MessageBox.Show("Please select a file that contains a list of UniProt IDs", "Warning message")
+        End If
+    End Sub
+
+    Public Sub Compress(ByVal directorySelected As DirectoryInfo, ByVal path2export As String)
+        Dim success_execution As Boolean = False
+        For Each fileToCompress As FileInfo In directorySelected.GetFiles()
+            If (fileToCompress.Name = "proteinAnnotations.txt") Then
+                Using originalFileStream As FileStream = fileToCompress.OpenRead()
+                    Try
+                        Using compressedFileStream As FileStream = File.Create(fileToCompress.FullName & ".gz")
+
+                            Using compressionStream As GZipStream = New GZipStream(compressedFileStream, CompressionMode.Compress)
+                                originalFileStream.CopyTo(compressionStream)
+                            End Using
+                        End Using
+
+                        Dim info As FileInfo = New FileInfo(path2export & Path.DirectorySeparatorChar & fileToCompress.Name & ".gz")
+                        success_execution = True
+                    Catch ex As Exception
+                        success_execution = False
+                    End Try
+                End Using
+                If success_execution Then
+                    My.Computer.FileSystem.DeleteFile(path2export & Path.DirectorySeparatorChar & fileToCompress.Name)
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Function GetAttibuteValue(ByVal node As XmlNode, ByVal attibutename As String) As String
+        Dim ret As String = String.Empty
+        If node IsNot Nothing AndAlso node.Attributes IsNot Nothing Then
+            Dim attrib As XmlNode = node.Attributes.GetNamedItem(attibutename)
+            If attrib IsNot Nothing Then
+                ret = attrib.Value
+            End If
+        End If
+        Return ret
+    End Function
+
+    Private Sub StopBtn_Click(sender As Object, e As EventArgs) Handles StopBtn.Click
+        stopClickPressed = True
+        filePath = ""
+        directoryPath = ""
+        FilePathTxtBox.Text = ""
+        MessageBox.Show("The process has stopped successfully", "Information message")
+    End Sub
+
+End Class
